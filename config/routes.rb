bba5a5aa3b4782e52b37d3d50052ba6293c737a6
boying.shi <shi.boying@gmail.com>
2018-06-17 21:02:52 -0700

    bs - setup entry point view for react app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'images#index'
+  root 'application#home'
 
-  resources 'images', only: %i[index new create show destroy]
+  resources :feedbacks, only: [:new]
 end
