a07a6bc4fdf13c94db0230328164cad9267aea37
Daniel Chen <chen.daniel@husky.neu.edu>
2019-05-21 14:33:22 -0700

    dc - Create home page

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  test 'should get home' do
+    get root_path
+    assert_response :ok
+    assert_select 'h1', 'Hello World'
+  end
+end
