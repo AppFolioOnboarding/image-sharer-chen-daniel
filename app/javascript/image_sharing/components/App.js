3837694f928b501935d5cce8d73f42d61d0facd5
Daniel Chen <chen.daniel@husky.neu.edu>
2019-05-28 17:01:52 -0700

    dc -Remove placeholder text

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div>
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
       <FeedbackForm />
       <Footer />
     </div>
