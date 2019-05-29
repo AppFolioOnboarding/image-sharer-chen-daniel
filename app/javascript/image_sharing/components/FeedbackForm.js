84c045b221970b1e4716f4f7eac3fbeca8e4c45b
Daniel Chen <chen.daniel@husky.neu.edu>
2019-05-29 16:01:35 -0700

    dc - Implement feedback form front end functionality

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,21 +1,108 @@
 import React from 'react';
-import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Container, Form, FormGroup, Label, Input, Button, FormFeedback, Alert } from 'reactstrap';
+import { observer } from 'mobx-react';
+import { observable, toJS, action } from 'mobx';
+import * as api from '../utils/helper';
 
+@observer
+export default class FeedbackForm extends React.Component {
+  @observable name = '';
+  @action setName = (value) => {
+    this.name = value;
+    this.nameFeedback = undefined;
+  }
 
-export default function FeedbackForm() {
-  return (
-    <Container>
-      <Form>
-        <FormGroup>
-          <Label for="feedbackName">Your Name</Label>
-          <Input type="text" name="name" id="feedbackName" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="feedbackComments">Comments</Label>
-          <Input type="textarea" name="comments" id="feedbackComments" />
-        </FormGroup>
-        <Button color="primary">Submit</Button>
-      </Form>
-    </Container>
-  );
+  @observable comments = '';
+  @action setComments = (value) => {
+    this.comments = value;
+    this.commentsFeedback = undefined;
+  }
+
+  @observable nameFeedback = undefined;
+  @observable commentsFeedback = undefined;
+
+  @observable alertMessage = undefined;
+  @observable alertColor = undefined;
+
+  @action
+  handleDismiss = () => {
+    this.alertMessage = undefined;
+  }
+
+  @action
+  submitForm = (e) => {
+    e.preventDefault();
+
+    this.alertMessage = undefined;
+
+    const data = {
+      name: toJS(this.name),
+      comments: toJS(this.comments)
+    };
+
+    return api.post('/api/feedbacks', data)
+      .then((res) => {
+        this.name = '';
+        this.comments = '';
+        this.alertMessage = res.message;
+        this.alertColor = 'info';
+      })
+      .catch((err) => {
+        if (err.data) {
+          if (err.data.name) {
+            this.nameFeedback = err.data.name;
+          }
+          if (err.data.comments) {
+            this.commentsFeedback = err.data.comments;
+          }
+        } else {
+          this.alertMessage = 'Something went wrong';
+          this.alertColor = 'danger';
+        }
+      });
+  };
+
+  render() {
+    return (
+      <Container>
+        {
+          this.alertMessage && (
+          <Alert
+            toggle={() => this.handleDismiss()}
+            color={this.alertColor}
+          >
+            {this.alertMessage}
+          </Alert>
+          )
+        }
+        <Form onSubmit={this.submitForm}>
+          <FormGroup>
+            <Label for="feedbackName">Your Name</Label>
+            <Input
+              type="text"
+              name="name"
+              id="feedbackName"
+              onChange={e => this.setName(e.target.value)}
+              value={this.name}
+              invalid={!!this.nameFeedback}
+            />
+            <FormFeedback>{this.nameFeedback}</FormFeedback>
+          </FormGroup>
+          <FormGroup>
+            <Label for="feedbackComments">Comments</Label>
+            <Input
+              type="textarea"
+              name="comments"
+              id="feedbackComments"
+              onChange={e => this.setComments(e.target.value)}
+              value={this.comments}
+              invalid={!!this.commentsFeedback}
+            />
+            <FormFeedback>{this.commentsFeedback}</FormFeedback>
+          </FormGroup>
+          <Button color="primary">Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
--- app/javascript/image_sharing/test/components/FeedbackForm.spec.js
+++ app/javascript/image_sharing/test/components/FeedbackForm.spec.js
@@ -1,13 +1,15 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
+import sinon from 'sinon';
 import FeedbackForm from '../../components/FeedbackForm';
+import * as api from '../../utils/helper';
 
 describe('<FeedbackForm />', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(<FeedbackForm />);
+    const wrapper = mount(<FeedbackForm />);
 
     const formGroup1 = wrapper.find('FormGroup').at(0);
     assert.strictEqual(formGroup1.length, 1);
@@ -22,5 +24,151 @@ describe('<FeedbackForm />', () => {
     const submitButton = wrapper.find('Button');
     assert.strictEqual(submitButton.length, 1);
     assert.strictEqual(submitButton.children().text(), 'Submit');
+  });
+
+  it('should successfully submit form and reset inputs', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    wrapper.instance().name = 'Daniel';
+    wrapper.instance().comments = 'Nice';
+    wrapper.instance().alertMessage = 'Error';
+
+    const e = { preventDefault: sinon.spy() };
+    const stub = sinon.stub(api, 'post').resolves({
+      message: 'Feedback successfully submitted'
+    });
+
+    return wrapper.instance().submitForm(e).then(() => {
+      assert(e.preventDefault.calledOnce);
+      assert(stub.calledOnceWithExactly('/api/feedbacks', { name: 'Daniel', comments: 'Nice' }));
+      assert.strictEqual(wrapper.instance().name, '');
+      assert.strictEqual(wrapper.instance().comments, '');
+      assert.strictEqual(wrapper.instance().alertMessage, 'Feedback successfully submitted');
+      assert.strictEqual(wrapper.instance().alertColor, 'info');
+      stub.restore();
+    });
+  });
+
+  it('should update UI with error fields on submit rejection', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    wrapper.instance().name = 'Daniel';
+    wrapper.instance().comments = '';
+    wrapper.instance().alertMessage = 'Feedback successfully submitted';
+
+    const e = { preventDefault: sinon.spy() };
+    const stub = sinon.stub(api, 'post').rejects({
+      data: {
+        comments: "can't be blank"
+      }
+    });
+
+    return wrapper.instance().submitForm(e).then(() => {
+      assert(e.preventDefault.calledOnce);
+      assert(stub.calledOnceWithExactly('/api/feedbacks', { name: 'Daniel', comments: '' }));
+      assert.strictEqual(wrapper.instance().name, 'Daniel');
+      assert.strictEqual(wrapper.instance().comments, '');
+      assert.strictEqual(wrapper.instance().nameFeedback, undefined);
+      assert.strictEqual(wrapper.instance().commentsFeedback, "can't be blank");
+      assert.strictEqual(wrapper.instance().alertMessage, undefined);
+      stub.restore();
+    });
+  });
+
+  it('should alert failure if there is some other error', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    wrapper.instance().name = 'Daniel';
+    wrapper.instance().comments = 'This is a comment.';
+    wrapper.instance().alertMessage = 'Feedback successfully submitted';
+
+    const e = { preventDefault: sinon.spy() };
+    const stub = sinon.stub(api, 'post').rejects({
+      message: 'Failed to fetch'
+    });
+
+    return wrapper.instance().submitForm(e).then(() => {
+      assert(e.preventDefault.calledOnce);
+      assert(stub.calledOnceWithExactly('/api/feedbacks', {
+        name: 'Daniel',
+        comments: 'This is a comment.'
+      }));
+      assert.strictEqual(wrapper.instance().name, 'Daniel');
+      assert.strictEqual(wrapper.instance().comments, 'This is a comment.');
+      assert.strictEqual(wrapper.instance().nameFeedback, undefined);
+      assert.strictEqual(wrapper.instance().commentsFeedback, undefined);
+      assert.strictEqual(wrapper.instance().alertMessage, 'Something went wrong');
+      assert.strictEqual(wrapper.instance().alertColor, 'danger');
+      stub.restore();
+    });
+  });
+
+  it('should render Alert with alert message and color', () => {
+    const wrapper = mount(<FeedbackForm />);
+    let alert = wrapper.find('Alert');
+    assert.strictEqual(alert.length, 0);
+
+    wrapper.instance().alertMessage = 'This is our alert message';
+    wrapper.instance().alertColor = 'warriorsColors';
+    wrapper.update();
+
+    alert = wrapper.find('Alert');
+    assert.strictEqual(alert.length, 1);
+    assert.strictEqual(alert.prop('color'), 'warriorsColors');
+    assert.strictEqual(alert.children().text(), '×This is our alert message');
+  });
+
+  it('adding a verification error for name should display an error via FormFeedback', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    wrapper.instance().nameFeedback = "can't be blank";
+    wrapper.update();
+    const fb = wrapper.find('FormFeedback').at(0);
+    assert.strictEqual(fb.length, 1);
+    assert.strictEqual(fb.children().text(), "can't be blank");
+  });
+
+  it('adding a verification error for comments should display an error via FormFeedback', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    wrapper.instance().commentsFeedback = "can't be blank";
+    wrapper.update();
+    const fb = wrapper.find('FormFeedback').at(1);
+    assert.strictEqual(fb.length, 1);
+    assert.strictEqual(fb.children().text(), "can't be blank");
+  });
+
+  it('changes to the feedback input should be reflected in the state and UI', () => {
+    const wrapper = mount(<FeedbackForm />);
+
+    let nameInput = wrapper.find('#feedbackName').first();
+    assert.strictEqual(nameInput.prop('value'), '');
+    nameInput.simulate('change', { target: { value: 'Luke' } });
+    assert.strictEqual(wrapper.instance().name, 'Luke');
+    nameInput = wrapper.find('#feedbackName').first();
+    assert.strictEqual(nameInput.prop('value'), 'Luke');
+
+    let commentInput = wrapper.find('#feedbackComments').first();
+    commentInput.simulate('change', { target: { value: 'May the Force Push be with you' } });
+    assert.strictEqual(wrapper.instance().comments, 'May the Force Push be with you');
+    commentInput = wrapper.find('#feedbackComments').first();
+    assert.strictEqual(commentInput.prop('value'), 'May the Force Push be with you');
+  });
+
+  it('handleDismiss should reset alertMessage and remove alert from UI', () => {
+    const wrapper = mount(<FeedbackForm />);
+    wrapper.instance().alertMessage = 'This is an alert';
+    wrapper.instance().alertColor = 'danger';
+    wrapper.update();
+
+    let alert = wrapper.find('Alert');
+    assert.strictEqual(alert.length, 1);
+
+    wrapper.instance().handleDismiss();
+    wrapper.update();
+
+    assert.strictEqual(wrapper.instance().alertMessage, undefined);
+    alert = wrapper.find('Alert');
+    assert.strictEqual(alert.length, 0);
   });
 });
